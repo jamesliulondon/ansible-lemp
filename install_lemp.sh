396e79601e62e0760763b0838b840655c7b24a9d
James Liu <james.liu@performgroup.com>
2015-06-20 17:07:10 +0100

    commit bash method

--- install_lemp.sh
+++ install_lemp.sh
@@ -1,12 +1,22 @@
 set -x
-sudo rpm -Uvh http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
-sudo rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
+#sudo rpm -Uvh http://download.fedoraproject.org/pub/epel/6/i386/epel-release-6-8.noarch.rpm
+#sudo rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
+sudo yum install epel-release
 
-sudo yum install -y mysql mysql-server nginx php-fpm php-mysql
+sudo yum install -y mysql mysql-server nginx php-fpm
 sudo /etc/init.d/mysqld restart
 
+[[ $1 == "nophp" ]] || sudo yum install -y php-mysql
+BASEDIR=$(dirname $0)
 
 myIp=`ifconfig eth0 | sed -n '/inet addr.*Mask/s/.*inet addr:\(.*\) Bcast.*/\1/p'`
 
-sed -i 's/^cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/' /etc/php.ini
-cp default.conf /etc/php-fpm/conf.d/.
+sed -i 's/^;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/' /etc/php.ini
+cp ${BASEDIR}/default.conf /etc/nginx/conf.d/.
+sed -i 's/^user = apache$/user = nginx/' /etc/php-fpm.d/www.conf
+sed -i 's/^group = apache$/group = nginx/' /etc/php-fpm.d/www.conf
+sed -i 's/^worker_process[ ]*1/worker_processes  4;/' /etc/nginx/nginx.conf
+worker_processes  1;
+
+service nginx restart
+service php-fpm restart
